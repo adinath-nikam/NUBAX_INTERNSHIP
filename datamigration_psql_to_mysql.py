dcb0c7164deb439a77395c4f43244d16a0ee70ca
Adinath Nikam <adinathanikam@gmail.com>
2022-04-30 18:44:10 +0530

    Update datamigration_psql_to_mysql.py

--- datamigration_psql_to_mysql.py
+++ datamigration_psql_to_mysql.py
@@ -6,11 +6,10 @@
 # Source readonly
 # Table : beneficiaries
 # DB : beneficiary_db
-# psql_server = &quot;3.7.81.85&quot;
-# psql_username = &quot;dbreaduser&quot;
-# psql_password = &quot;DBPROD@read$#$&quot;
-# Destination :
-# setup mysql on localhost and write in beneficiary_db (create new db on localhost through code)
+# psql_server = "3.7.81.85"
+# psql_username = "dbreaduser"
+# psql_password = "DBPROD@read$#$"
+# Destination : setup mysql on localhost and write in beneficiary_db (create new db on localhost through code)
 
 
 import psycopg2 as pg
